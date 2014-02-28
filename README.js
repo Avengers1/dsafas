1de18d7a2eff6a3a7a970a9436ce058feeafdb13
Avengers1 <mateus_vicentin2011@hotmail.com>
2014-02-28 19:44:24 -0200

    Update and rename README.md to README.js

---
+++ README.js
@@ -0,0 +1,57 @@
+//Throw this in your console tab, F12 at browser then console tab. Refresh your page to unload the script.
+//commands = /uid @:name:, example: /uid @Isly | chat reply 'User @Isly statistics: djPoints: 10691 | listenerPoints: 11415 | fans: 289 | grabPoints: 513'
+API.on(API.CHAT_COMMAND, command); 
+function command(value) {
+
+	if(value.indexOf("/uid @") >=0)
+	{
+		userIdCommand(value);
+	}
+}
+ 
+	function userIdCommand(data)
+	{
+		var name = data.substr(6).trim();
+		var pos = checkUsername(name);
+		if(pos != null){
+			var id = getUserId(name);
+			var points = API.getUser(id).djPoints;
+	   		var listenerPoints = API.getUser(id).listenerPoints;
+	   		var fans = API.getUser(id).fans;
+	   		var curatorPoints = API.getUser(id).curatorPoints;
+			API.chatLog("UID for " + name + " is: " + id);
+			API.chatLog("User @" + name + " statistics: djPoints: " + points + " | listenerPoints: " + listenerPoints + " | fans: " + fans + " | grabPoints: " + curatorPoints);
+		}
+	}
+
+	function checkUsername(name)
+	{
+		var userlist = API.getUsers();
+	
+		for(var i=0; i < userlist.length; i++)
+		{
+			if(userlist[i].username == name)
+			{
+				var pos = i;
+				break;
+			}else{
+				var pos = null;
+			}
+		}
+		return pos;
+	}
+
+	function getUserId(name)
+	{
+		var userlist = API.getUsers();
+	
+		for(var i=0; i < userlist.length; i++)
+		{
+			if(userlist[i].username == name)
+			{
+				var userid = userlist[i].id;
+				break;
+			}
+		}
+		return userid;
+	}
--- README.md
+++
@@ -1,4 +0,0 @@
-dsafas
-======
-
-fsafsafsafas
